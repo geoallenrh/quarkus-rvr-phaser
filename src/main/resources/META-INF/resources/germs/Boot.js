3e22b3aa25b73010f08bf47f0e1bf0053c3e543e
geoallenrh <geoffallen@redhat.com>
2021-09-27 22:21:08 -0500

    changed the fontmap

--- src/main/resources/META-INF/resources/germs/Boot.js
+++ src/main/resources/META-INF/resources/germs/Boot.js
@@ -9,7 +9,7 @@ export default class Boot extends Phaser.Scene
     {
         this.load.setPath('assets/games/rvr/');
         this.load.image('background', 'background.jpg');
-        this.load.bitmapFont('slime', 'slime-font.png', 'slime-font.xml');
+        this.load.bitmapFont('slime', 'rvr-font.png', 'rvr-font.xml');
     }
 
     create ()
